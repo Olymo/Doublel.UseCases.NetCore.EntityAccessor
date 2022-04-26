1d43208c6656b5dbf38e47fb236f133b24fc26b4
ivanpetric998 <ivan.98ets@gmail.com>
2022-04-26 12:11:47 +0200

    add detete handler

---
+++ src/Doublel.UseCases.NetCore.EntityAccessor/EfGenericDeleteHandler.cs
@@ -0,0 +1,24 @@
+﻿using Dobulel.UseCases.EntityAccessor;
+using Doublel.EntityAccessor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doublel.UseCases.NetCore.EntityAccessor
+{
+    public class EfGenericDeleteHandler<TUseCase, TEntity> : EfCommandUseCaseHandler<TUseCase, int, TEntity>
+        where TUseCase : UseCase<int, Empty>
+        where TEntity : Entity
+    {
+        public EfGenericDeleteHandler(Doublel.EntityAccessor.EntityAccessor accessor) : base(accessor)
+        {
+        }
+
+        public override Empty Handle(TUseCase useCase)
+        {
+            Accessor.Remove<TEntity>(useCase.Data);
+            Accessor.Commit();
+            return Empty.Value;
+        }
+    }
+}
